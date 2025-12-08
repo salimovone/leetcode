80a70d8c67b57b6da9c0e1633e6ecda31ffc14fc
salimovone <salimovone@DESKTOP-02FPF0R>
2025-12-08 14:37:09 +0500

    problem 8 is solved

--- 8.string_to_int/main.c
+++ 8.string_to_int/main.c
@@ -1,50 +1,103 @@
 #include <stdio.h>
+#include <limits.h>
 
-int myAtoi(char *s) {
-  int res = 0;
-  for (char *p = s; *p != '\0'; p++) {
-    switch (*p) {
-    case '0':
-      if (p[0] != '0')
-        break;
-    case '1':
-      break;
-    case '2':
-      break;
-    case '3':
-      break;
-    case '4':
-      break;
-    case '5':
-      break;
-    case '6':
-      break;
-    case '7':
-      break;
-    case '8':
-      break;
-    case '9':
-      break;
-    case '-':
-      break;
-    case '+':
-      break;
-    case ' ':
-      break;
-    default:
-      break;
-    }
+int isNegative = 0;
+int isBreak = 0;
 
-    return res;
-  }
+void addDigit(int* res, int digit) {
+	if (*res > (INT_MAX - digit) / 10) {
+		if (isNegative)
+		{
+			*res = INT_MIN;
+			isNegative = 0;
+		}
+		else
+			*res = INT_MAX;
+		isBreak = 1;
+		return;
+	}
+
+	*res = (*res) * 10 + digit;
+}
+
+int myAtoi(char* s) {
+	int res = 0;
+	int isOnlyWhiteSpace = 0;
+	int isFirst = 0;
+	isNegative = 0;
+	isBreak = 0;
+	for (int i = 0; s[i] != '\0'; i++) {
+		switch (s[i]) {
+		case '0':
+			addDigit(&res, 0);
+			break;
+		case '1':
+			addDigit(&res, 1);
+			break;
+		case '2':
+			addDigit(&res, 2);
+			break;
+		case '3':
+			addDigit(&res, 3);
+			break;
+		case '4':
+			addDigit(&res, 4);
+			break;
+		case '5':
+			addDigit(&res, 5);
+			break;
+		case '6':
+			addDigit(&res, 6);
+			break;
+		case '7':
+			addDigit(&res, 7);
+			break;
+		case '8':
+			addDigit(&res, 8);
+			break;
+		case '9':
+			addDigit(&res, 9);
+			break;
+		case '-':
+			if ((i == 0 || isOnlyWhiteSpace == i) && !isFirst) {
+				isNegative = 1;
+				isFirst = 1;
+				isOnlyWhiteSpace++;
+			}
+			else isBreak = 1;
+			break;
+		case '+':
+			if ((i == 0 || isOnlyWhiteSpace == i) && !isFirst) {
+				isNegative = 0;
+				isFirst = 1;
+				isOnlyWhiteSpace++;
+			}
+			else isBreak = 1;
+			break;
+		case ' ':
+			if (isOnlyWhiteSpace != i || isFirst) isBreak = 1;
+			isOnlyWhiteSpace++;
+			break;
+		default:
+			isBreak = 1;
+			break;
+		}
+		if (isBreak) break;
+	}
+	return isNegative ? -res : res;
 }
 
 int main() {
-  printf("-042: %d\n", myAtoi("-042"));
-  printf("42: %d\n", myAtoi("42"));
-  printf("1337c0d3: %d\n", myAtoi("1337c0d3"));
-  printf("0-1: %d\n", myAtoi("0-1"));
-  printf("words and 987: %d\n", myAtoi("words and 987"));
+	//printf("-042: %d\n", myAtoi("-042"));
+	//printf("   -042: %d\n", myAtoi("   -042"));
+	//printf("-04121-2: %d\n", myAtoi("-04121-2"));
+	//printf("42: %d\n", myAtoi("42"));
+	//printf("1337c0d3: %d\n", myAtoi("1337c0d3"));
+	//printf("words and 987: %d\n", myAtoi("words and 987"));
+	//printf("0-1: %d\n", myAtoi("0-1"));
+	//printf(" - 91283472332: %d\n", myAtoi("-91283472332"));
+	//printf("   -+6 123: %d\n", myAtoi("   -+6 123"));
+	printf("  +  413: %d\n", myAtoi("  +  413"));
 
-  return 0;
+	return 0;
 }
